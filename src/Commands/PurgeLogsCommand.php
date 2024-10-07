883dcf5eee23e19f6d4c855b06f80bbb7d460994
fotsodarwin <fotsodarwin@gmail.com>
2024-10-07 02:23:57 +0100

    Update the package description

--- src/Commands/PurgeLogsCommand.php
+++ src/Commands/PurgeLogsCommand.php
@@ -12,53 +12,44 @@ class PurgeLogsCommand extends Command
 
     public function handle()
     {
-        // Récupération du chemin des logs
         $logPath = storage_path('logs');
-        //Verification de keep-days
+
         $keepDays = (int) $this->option('keep-days') ?: config('purge-logs.retention_period');
 
-        // Vérification que le répertoire des logs existe
         if (!File::exists($logPath)) {
             $this->error("Le répertoire des logs n'existe pas.");
             return;
         }
 
-        // Liste des fichiers logs dans le répertoire
         $logFiles = File::files($logPath);
         $now = now();
 
         foreach ($logFiles as $logFile) {
             $this->info("Traitement du fichier : " . $logFile->getFilename());
-            
-            // Lire le contenu du fichier
+
             $lines = file($logFile->getPathname(), FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
             $newContent = [];
             $currentLogDate = null;
 
             foreach ($lines as $line) {
-                // Vérifier si la ligne contient une date (par exemple [2024-10-05 12:00:00])
+                //verify if the line contains a date (i.e [2024-10-05 12:00:00])
                 if (preg_match('/\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\]/', $line, $matches)) {
-                    // Si une date est trouvée, la convertir en objet Carbon
                     $logDate = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $matches[1]);
 
-                    // Si le log date d'il y a plus de `keep-days` jours, on ne l'ajoute pas
                     if ($logDate->diffInDays($now) > $keepDays) {
                         $currentLogDate = $logDate;
                         $this->info("Purge du log datant du : " . $logDate->toDateTimeString());
-                        continue; // Ignorer cette ligne et toutes les suivantes jusqu'au prochain log
+                        continue;
                     }
 
-                    // Sinon, mettre à jour la date courante et ajouter la ligne au contenu à conserver
                     $currentLogDate = null;
                 }
 
-                // Si aucune purge en cours, ajouter la ligne
                 if (!$currentLogDate) {
                     $newContent[] = $line;
                 }
             }
 
-            // Réécrire le fichier avec le contenu mis à jour
             File::put($logFile->getPathname(), implode(PHP_EOL, $newContent));
             $this->info("Purge terminée pour : " . $logFile->getFilename());
         }
